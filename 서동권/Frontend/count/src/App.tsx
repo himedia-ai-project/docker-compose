cee7f2b6a02e5f61f5498e907baa6bc6d2ff757d
RudySeo <gun100491@gmail.com>
2025-03-13 21:46:20 +0900

    Force commit after clearing cache

---
+++ 서동권/Frontend/count/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Count from "./Components/Count";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>숫자 증가</h1>
+      <Count />
+    </div>
+  );
+}
+
+export default App;

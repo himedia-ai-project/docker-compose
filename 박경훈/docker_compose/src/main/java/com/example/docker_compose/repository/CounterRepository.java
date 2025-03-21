085125a9640a444f5624616c738a1b26d0ceb2de
박경훈 <khlove0916@naver.com>
2025-03-21 20:39:02 +0900

    Delete 박경훈/docker_compose directory

--- 박경훈/docker_compose/src/main/java/com/example/docker_compose/repository/CounterRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.docker_compose.repository;
-
-import com.example.docker_compose.model.Counter;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CounterRepository extends JpaRepository<Counter, Long> {
-}
